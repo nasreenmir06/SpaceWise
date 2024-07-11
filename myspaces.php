9790e4eda5d618b4563e43f56804edf18667d0dc
nasreenmir06 <nasreenmir06@gmail.com>
2024-07-11 18:28:01 -0400

    Updated files

---
+++ myspaces.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Spaces</title>
+    <link rel="stylesheet" href="style.css?v=0.0">
+</head>
+<body>
+    <h1>My Spaces</h1>
+</body>
+</html>
